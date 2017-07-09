5956680fcfca975f5452bce9d5c7e66fdeeeee9e
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 06:09:55 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.Camera.Ps.Core/IRevisionProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.Camera.Ps;
+using Net.Chdk.Meta.Model.CameraList;
+using Net.Chdk.Meta.Model.CameraTree;
+
+namespace Net.Chdk.Meta.Providers.Camera.Ps
+{
+    public interface IRevisionProvider
+    {
+        RevisionData GetRevision(string revision, TreeRevisionData treeRevision, ListPlatformData list, TreePlatformData tree);
+    }
+}
