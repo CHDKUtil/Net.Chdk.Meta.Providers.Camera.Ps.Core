27f443d3255898dc050923f410a446c43870cbfa
Dmitry Shechtman <damnian@gmail.com>
2019-09-02 23:37:35 +0300

    Move GetRevisions() to IRevisionProvider

--- src/Net.Chdk.Meta.Providers.Camera.Ps.Core/IRevisionProvider.cs
+++ src/Net.Chdk.Meta.Providers.Camera.Ps.Core/IRevisionProvider.cs
@@ -1,4 +1,5 @@
-﻿using Net.Chdk.Meta.Model.Camera.Ps;
+﻿using System.Collections.Generic;
+using Net.Chdk.Meta.Model.Camera.Ps;
 using Net.Chdk.Meta.Model.CameraList;
 using Net.Chdk.Meta.Model.CameraTree;
 
@@ -6,6 +7,6 @@ namespace Net.Chdk.Meta.Providers.Camera.Ps
 {
     public interface IRevisionProvider
     {
-        RevisionData GetRevision(string revision, TreeRevisionData treeRevision, ListPlatformData list, TreePlatformData tree, string productName);
+        IDictionary<string, RevisionData> GetRevisions(string productName, ListPlatformData list, TreePlatformData tree);
     }
 }
